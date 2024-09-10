e2e630e3e7d862a5b28e499940d637909d39bb42
Vatsal Goel <vatsal.goel@utah.edu>
2024-09-09 19:16:53 -0600

    Added dns header structure to response

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket
+import struct
 
 
 def main():
@@ -14,7 +15,29 @@ def main():
         try:
             buf, source = udp_socket.recvfrom(512)
     
-            response = b""
+            # Building dns header structure
+            # ID assigned to query packets
+            packet_id = 1234
+
+            # Header Flags            
+            qr = 1
+            opcode = 0
+            aa = 0
+            tc = 0
+            rd = 0
+            ra = 0
+            z = 0
+            rcode = 0
+
+            flags = (qr << 15) | (opcode << 11) | (aa < 10) | (tc << 9) | (rd << 8) | (ra << 7) | (z << 4) | rcode
+
+            # Questions and records section
+            qdcount = 0
+            ancount = 0
+            nscount = 0
+            arcount = 0
+
+            response = struct.pack("HHHHHH!", packet_id, flags, qdcount, ancount, nscount, arcount)
     
             udp_socket.sendto(response, source)
         except Exception as e:
